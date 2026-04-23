0dc3a4cf530cd423145689ad6813e1fa1ffe4529
RobertL31 <uhc.contact@laposte.net>
2026-04-23 21:31:56 +0200

    update app name in header

--- src/renderer/components/Misc/Header.tsx
+++ src/renderer/components/Misc/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
         <div>
             <div className='w-full h-24 bg-slate-200 border border-black border-opacity-20 shadow-md flex overflow-hidden pr-5 justify-between'>
                 <p className='self-center ml-5 font-mono font-bold text-2xl'>
-                    IPC Analyzer App
+                    ProvScope App
                 </p>
                 <img 
                     src={goldoIcon} 
