867a5ff3c96dc6a3257084b345981c854357282a
RobertL31 <uhc.contact@laposte.net>
2026-06-11 15:15:03 +0200

    More informative init and clean scripts

--- clean.bash
+++ clean.bash
@@ -2,3 +2,5 @@
 
 rm -rf ./node_modules
 rm -rf ./venv
+
+echo "All dependencies have been removed. You can run init.bash to setup the project again."
--- init.bash
+++ init.bash
@@ -4,10 +4,11 @@ setup() {
     echo "Installing React app dependencies"
     npm install
 
-    echo "Installing Python dependencies"
+    echo "Creating local venv and installing Python dependencies"
     python3 -m venv ./venv
     source ./venv/bin/activate
     pip install -r ./requirements.txt
+    pip install -e .
 }
 
 ASK="Would you like to download and setup dependencies for this project ? (Y/n) "
@@ -15,4 +16,6 @@ read -p "$ASK" -r
 if [[ "$REPLY" =~ ^[Yy]$ || "$REPLY" == "" ]]
 then
     setup
+else
+    echo "Aborting setup. Please run this script again to setup dependencies when you are ready."
 fi
